4849c1e2b7476416b1016344f6dddcfa8e733cdc
AjitSingh <jeetsingh.ajit@gmail.com>
2016-09-18 19:24:29 +0530

    adding ability to get info of a group of pipelines

--- lib/gocd/pipelines.rb
+++ lib/gocd/pipelines.rb
@@ -26,4 +26,37 @@ module GOCD
       end
     end
   end
+
+  class PipelineGroup
+    def initialize(pipelines)
+      @pipelines = pipelines
+    end
+
+    def information_available?
+      !pipelines.nil?
+    end
+
+    def red_pipelines
+      pipelines.select { |pipeline| pipeline.red? }
+    end
+
+    def green_pipelines
+      pipelines.select { |pipeline| pipeline.green? }
+    end
+
+    def status
+      pipelines.map { |pipeline| pipeline.status }
+    end
+
+    def any_red?
+      !red_pipelines.empty?
+    end
+
+    def pipelines
+      all_pipelines = GOCD::PipelineRepository.pipelines
+      @pipelines.map do |pipeline|
+        all_pipelines.find { |p| p.name == pipeline }
+      end
+    end
+  end
 end
